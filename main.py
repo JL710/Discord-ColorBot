8647c55f8e90560e2d4f2ff0500c80fbb16140af
JL710 <76447362+JL710@users.noreply.github.com>
2023-05-22 19:02:21 +0200

    console logging

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 
 class Bot(commands.Bot):
     async def on_ready(self):
         print(f"Bot {self.user} started!")
+
+    async def on_application_command(self, ctx: discord.ApplicationContext, *args, **kwargs):
+        print(datetime.now(), ctx.author.id, ctx.interaction.data)
 
 bot = Bot(owner_id=398440299627544577)
 
